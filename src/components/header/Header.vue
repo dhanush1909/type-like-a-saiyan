563175647dd3dfa0def954ac70bf476d998e6072
dhanush.kumar <dsubbareddy@afs.net>
2021-08-21 02:00:29 +0530

    initial pages set up along with header and router

---
+++ src/components/header/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <router-link to="/"><i class="el-icon-s-home nav-icon"></i></router-link>
+    <router-link to="/type"
+      ><i class="el-icon-monitor nav-icon"></i
+    ></router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss">
+.header {
+  position: sticky;
+  height: 80px;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+
+  a {
+    color: #eeeeee !important;
+    margin: 0 10px;
+
+    .nav-icon {
+      font-size: 25px;
+    }
+  }
+
+  .nav-icon:hover {
+    color: #f6c90e !important;
+  }
+}
+</style>
---
+++ src/views/Type.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="type">suppppppppppppppppppppppppppppppppppp</div>
+</template>
+
+<script>
+export default {
+  name: "Type",
+};
+</script>
