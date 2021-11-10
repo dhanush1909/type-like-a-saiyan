a9f210e4be336a36c1983d5394e7a2b433208bb4
Dhanush Kumar <dhanush1926@gmail.com>
2021-11-10 20:30:42 +0530

    welcome message in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <div class="logo-container">
       <img alt="Vue logo" src="../assets/logo.png" />
+      <br />
+      <br />
+      <div>Created with love by DK. TLS is a minimalistic typing test.</div>
     </div>
   </div>
 </template>
@@ -21,6 +24,8 @@ export default {
 
   .logo-container {
     height: 50%;
+    min-height: 300px;
+    text-align: center;
 
     img {
       height: 100%;
